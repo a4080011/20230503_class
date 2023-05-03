dda3bd4c2519e8b53df2a4f54c32428e63e01c70
rwa <rwa@example.com.tw>
2023-05-03 16:29:24 +0800

    盎司轉克

--- oz_to_gram_class.py
+++ oz_to_gram_class.py
@@ -1,12 +1,11 @@
 class OzToGram:
-    def __init__(self,isSwim):
-        print("is swim "+str(isSwim))
-    def glod_oz(self,glodoz):
-        float(glodoz)
-        print(glodoz*31.1)
-    def normal_oz(self,normaloz):
-        float(normaloz)
-        print(normaloz*28.3)
-a = OzToGram(False)
-a.glod_oz(10)
-a.normal_oz(10)
+    def __init__(self,type):
+        if type == 1:
+            self.constant = 31
+        else:
+            self.constant = 28
+    def get_data(self,oz):
+        return oz*self.constant
+ozToGram = OzToGram(1)
+ozToGram.constant = 50
+print(ozToGram.get_data(1))
